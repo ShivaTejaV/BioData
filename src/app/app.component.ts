e26dcdfdc039660f23bf7387e252395821d73c55
Shiva Teja <vddshivateja@gmail.com>
2025-02-03 02:19:10 +0530

    done ng build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule for NgFor
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],  // Include CommonModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'BioData';
+
+  photos: string[] = [];
+
+  gpPhoto4: string = "https://lh3.googleusercontent.com/pw/AP1GczODq9Bl1sfJkz2p_Sq9hDSg4bvwdEsnvaZ2PZiAM3z4oPL_ySeHU7MJzVZI6J9OL3XMNrMWEX2e-4v78hIrBb8h104bNo_IlB_acN1gEb3NtmZXaBGkHSYO6n9EJSO71qn8dyFOrzKeLEwf17lmKTJz=w391-h607-s-no-gm?authuser=0";
+  gpPhoto3: string = "https://lh3.googleusercontent.com/pw/AP1GczPndReHz1aXlMcxxmWcU1B6Ho18gLbJoHdCTV4vQv6GHv-J51tAiUIojN9wwptwwKe_m__o7yb-k_TrZAm35XpRbrJgUmxoXjSZE13KwjAVduWWtFMTH1dNMGBQDq579O_EoTS52Q8wM0nZhOSLAFsl=w390-h878-s-no-gm?authuser=0";
+  gpPhoto2: string = "https://lh3.googleusercontent.com/pw/AP1GczPquATlgS7XQbut0t66jS6cwngYEjUmWqbzvxEXI-NgHnCAqqZymSzSF3PW1vIAS7IWndXeknhO7u8Jssr59xPNVYQMpSjMIBZZrChfDd54vXorqOa47z94NWSUkgoiLgpHSXcAWWW6tZabiWyb-WdC=w660-h878-s-no-gm";
+  gpPhoto1: string = "https://lh3.googleusercontent.com/pw/AP1GczPb8FTHaSqU4CHXL0uNz2LGydKxbeYU5sjHLgKljyncWRE9nLcS_OyzyLzjknlfSh-ZV4kGNlmXBSE_KuMvN8h7brRtHKxPTVKCfr7l8sESVATm6QddXztrDxlQvj-LyAi8qzL0KbKhy0HgTa2iywLh=w494-h878-s-no-gm";
+
+  ngOnInit() {
+    this.photos.push(this.gpPhoto1, this.gpPhoto2, this.gpPhoto3, this.gpPhoto4);
+  }
 }
